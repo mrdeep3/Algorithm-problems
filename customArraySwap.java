47bf9c018cd93d54145e9c2282a2c2d9a55a994a
Deep B Chitroda <43362100+mrdeep3@users.noreply.github.com>
2020-01-08 02:20:59 -0500

    Update customArraySwap.java
    added comments

--- customArraySwap.java
+++ customArraySwap.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 /**
  * Program to swap integers in array such that even number precedes odd numbers in minimum swaps
  * [12,1,17,6,9] should o/p to = [12,6,17,1,9] => ans is 1 as it took 1 swap to sort the array in reqd way. [6,12,17,1,9] is also valid
@@ -8,10 +6,11 @@ import java.util.Arrays;
  */
 public class customArraySwap 
 {
+	//function to check if the desired array is formed
 	public static boolean checkArrayFormat(int[] outputArr)
 	{
 		int odd_count = 0, even_count = 0;
-		for(int i = 0; i<outputArr.length; i++)
+		for(int i = 0; i<outputArr.length; i++) // to check for even numbers at the start
 		{
 			if(outputArr[i] %2 == 0)
 			{
@@ -22,7 +21,7 @@ public class customArraySwap
 				break;
 			}
 		}
-		for(int j = outputArr.length-1; j > 0; j--)
+		for(int j = outputArr.length-1; j > 0; j--) // to check for odd numbers at the end
 		{
 			if(outputArr[j ]%2 != 0)
 			{
@@ -44,17 +43,16 @@ public class customArraySwap
 		int count=0, temp = 0;
 		for(int i = 0; i < arr.length; i++)
 		{
-			if(arr[i] % 2 != 0)
+			if(arr[i] % 2 != 0) // if odd number found from left side of array, swap
 			{
 				for(int j = arr.length-1; j > 0; j--)
 				{
-					if(arr[j] % 2 == 0)
+					if(arr[j] % 2 == 0) // if even number found from right side of array, swap
 					{
 						temp = arr[j];
 						arr[j] = arr[i];
 						arr[i] = temp;
 						count++;
-						//j++;
 						break;
 					}
 				}
@@ -66,7 +64,8 @@ public class customArraySwap
 				break;
 			}
 		}
-		for (int i : arr) {
+		for (int i : arr) // print the swapped array
+		{
 			System.out.print(i+",");
 		}
 		System.out.println();
@@ -75,8 +74,7 @@ public class customArraySwap
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr = new int[] {1,2,3,4, 10}; //ans for this shud be 2
+		int[] arr = new int[] {1,2,3,4, 10, -1,0}; 
 		System.out.println("the count is "+swap(arr));
 	}
-
 }
